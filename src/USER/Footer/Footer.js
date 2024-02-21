780661e1eaf0655f32688135ec1ba4b94de9d79a
ABHISHEK KUMAR <100033618+abhishek110803@users.noreply.github.com>
2024-02-22 01:49:39 +0530

    Updated Home page

--- src/USER/Footer/Footer.js
+++ src/USER/Footer/Footer.js
@@ -2,24 +2,33 @@ import React from 'react';
 import './Footer.css';
 import { MdOutlineModeEdit } from "react-icons/md";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTelegram,faFacebook,faYoutube,faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faHeartO, faFacebook, faYoutube, faInstagram } from '@fortawesome/free-brands-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+
 
 
 const Footer = () => {
   return (
     <footer id="footer" className="footer">
       <ul className="ulik" style={{ listStyle: 'none' }} id="C">
-        <li className="lin"><a className="anchor" href="https://www.facebook.com/utkansh" target="_blank"><FontAwesomeIcon icon={faFacebook} size="3x" style={{ color: 'black' }}/></a></li>
-        <li className="lin"><a className="anchor" href="https://www.youtube.com/@UtkanshNITJalandhar" target="_blank"><FontAwesomeIcon icon={faYoutube} size="3x" style={{ color: 'black' }}/></a></li>
-        <li><a className="anchor" href="https://www.instagram.com/utkansh.nitj/" target="_blank"> <FontAwesomeIcon icon={faInstagram} size="3x" style={{ color: 'black' }} /></a></li>
+        <li className="lin"><a className="anchor" href="https://www.facebook.com/utkansh" target="_blank"><FontAwesomeIcon icon={faFacebook} size="1x" style={{ color: 'blue' }} /></a></li>
+        <li className="lin"><a className="anchor" href="https://www.youtube.com/@UtkanshNITJalandhar" target="_blank"><FontAwesomeIcon icon={faYoutube} size="1x" style={{ color: 'red' }} /></a></li>
+        <li><a className="anchor" href="https://www.instagram.com/utkansh.nitj/" target="_blank"> <FontAwesomeIcon icon={faInstagram} size="1x" style={{ color: 'red' }} /></a></li>
         {/* <li><a className="anchor" href="https://www.linkedin.com/in/abhishek-kumar-992757211/"><FontAwesomeIcon icon={faTelegram} size="3x"/></a></li> */}
         {/* <li><a className="anchor" href="https://www.instagram.com/abhishek1108032022/"><FontAwesomeIcon icon={faTelegram} size="3x"/></a></li> */}
       </ul>
-      {/* <br /> */}
-      <div>Created By Team Utkansh-24</div>
-      <div> 2024 All rights reserved.</div>
+      {/* <div class="container"> */}
+        <div class="row footer_bottom justify-content-center">
+          <p class="">
+            Made with <FontAwesomeIcon icon={faHeart} /> by <a href="">WEB AND APP TEAM UTKANSH</a>
+            <br></br> Copyright &copy; Utkansh
+            <script>document.write(new Date().getFullYear());</script>
+          </p>
+        </div>
+      {/* </div> */}
 
-      
+
     </footer>
   );
 };
--- src/USER/Pro-Night/Pronightmainpage.jsx
+++ src/USER/Pro-Night/Pronightmainpage.jsx
@@ -14,14 +14,18 @@ class ProNightSection extends React.Component {
             <>
                 {/* pro-night */}
                 {/* <div id="pro-night"> */}
-                <h2 className="titl" >
-                    Pro Night
+                <h2 className="titlp" >
+                    Pro Nite
                 </h2>
                 <div className='containerpro'>
                 <video src={videoFile} className="pro_video" loop autoPlay muted />
                 {/* <h1 className="pronighthome">ProNight</h1> */}
                
-                <button className='revelbutton' onClick={this.handleClick}>Reveal</button>
+                <button className='revelbutton1' onClick={this.handleClick}>Live Band</button>
+                <button className='revelbutton2' onClick={this.handleClick}>Kavi Sammelan</button>
+                <button className='revelbutton3' onClick={this.handleClick}>Bollywood Dance Music</button>
+                <button className='revelbutton4' onClick={this.handleClick}>Faishon Show</button>
+                <button className='revelbutton5' onClick={this.handleClick}>Star Night</button>
                 {/* </div> */}</div>
             </>
         );
