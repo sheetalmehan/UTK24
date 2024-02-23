72d0f123cdf257c7edb26ea7efa4013c36a68136
Kunal Kumar <kunal23kmr@gmail.com>
2024-02-24 01:55:35 +0530

    Done with logo link.

--- src/USER/navbar/Navbar.js
+++ src/USER/navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'; // Import Link
+import { Link, useNavigate } from 'react-router-dom'; // Import Link
 import './nav.css';
 import logoImage from './Group 59.svg'; // Import SVG image
 import Layout from '../../Layout/Layout';
@@ -16,7 +16,10 @@ const Navbar = () => {
             window.removeEventListener('scroll', handleScroll);
         };
     }, []);
-
+    const navigate = useNavigate();
+    const handleLogoClick = () => {
+        navigate('/');
+    }
     const handleScroll = () => {
         setSticky(window.scrollY > 20);
         setShowScrollBtn(window.scrollY > 500);
@@ -35,19 +38,19 @@ const Navbar = () => {
             <div className="max-widthnav">
 
                 <div className="logo">
-                    <img className="mainlogo" src={logoImage} alt="Logo" />
+                    <img className="mainlogo" onClick={handleLogoClick} src={logoImage} alt="Logo" />
                 </div>
-                <ul className={ 'menu lafda'} style={{marginRight:'45px'}}>
-                    <li><Link style={{textDecoration:'none'}} to="/" className="menu-btn">Home</Link></li>
-                    <li><Link style={{textDecoration:'none'}} to="/Gallery" className="menu-btn">Gallery</Link></li>
-                    <li><Link style={{textDecoration:'none'}} to="/clubsandevent" className="menu-btn">Events</Link></li>
-                    <li><Link style={{textDecoration:'none'}} to="/megaevent" className="menu-btn">Mega Events</Link></li>
-                    <li><Link style={{textDecoration:'none'}} to="/merchandise" className="menu-btn">Merchandise</Link></li>
-                    <li><Link style={{textDecoration:'none'}} to="/accomodationPage" className="menu-btn">Accommodation</Link></li>
-                    <li><Link style={{textDecoration:'none'}} to="/TeamMembers" className="menu-btn">Team Members</Link></li>
-                    <li><Link style={{textDecoration:'none'}} to="/FacultyCoordinators" className="menu-btn">Faculty Coordinator </Link></li>
-                    <li><Link style={{textDecoration:'none'}} to="/Developers" className="menu-btn">Developers</Link></li>
-                    <li><Link style={{textDecoration:'none'}} to="/Contacts" className="menu-btn">Contact us</Link></li>
+                <ul className={'menu lafda'} style={{ marginRight: '45px' }}>
+                    <li><Link style={{ textDecoration: 'none' }} to="/" className="menu-btn">Home</Link></li>
+                    <li><Link style={{ textDecoration: 'none' }} to="/Gallery" className="menu-btn">Gallery</Link></li>
+                    <li><Link style={{ textDecoration: 'none' }} to="/clubsandevent" className="menu-btn">Events</Link></li>
+                    <li><Link style={{ textDecoration: 'none' }} to="/megaevent" className="menu-btn">Mega Events</Link></li>
+                    <li><Link style={{ textDecoration: 'none' }} to="/merchandise" className="menu-btn">Merchandise</Link></li>
+                    <li><Link style={{ textDecoration: 'none' }} to="/accomodationPage" className="menu-btn">Accommodation</Link></li>
+                    <li><Link style={{ textDecoration: 'none' }} to="/TeamMembers" className="menu-btn">Team Members</Link></li>
+                    <li><Link style={{ textDecoration: 'none' }} to="/FacultyCoordinators" className="menu-btn">Faculty Coordinator </Link></li>
+                    <li><Link style={{ textDecoration: 'none' }} to="/Developers" className="menu-btn">Developers</Link></li>
+                    <li><Link style={{ textDecoration: 'none' }} to="/Contacts" className="menu-btn">Contact us</Link></li>
                 </ul>
             </div>
             <button className={showScrollBtn ? 'scroll-up-btn show' : 'scroll-up-btn'} onClick={scrollToTop}>
