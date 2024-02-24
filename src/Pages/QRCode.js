2e3354dbd51ddc6a825b575b6f21586f23b403d1
Kunal Kumar <kunal23kmr@gmail.com>
2024-02-24 12:43:55 +0530

    QR code and pronight video url updated.

--- src/Pages/QRCode.js
+++ src/Pages/QRCode.js
@@ -1,16 +1,17 @@
 import React from "react";
 import qr from './qrCode.jpg';
 
-const QRPopup = ({ setPopup }) => {
+const QRPopup = ({ setPopup, amount }) => {
     const handleCloseClick = () => {
         // Close the popup
         setPopup(false);
     };
 
     return (
-        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75 z-index-100" style={{zIndex:'3'}}> 
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75 z-index-100" style={{ zIndex: '3' }}>
             <div className="bg-white rounded-lg p-8 max-w-md flex flex-col items-center">
-                <img src={qr} alt="QR Code" className="w-full mb-4" style={{ height: '45vh', width:'auto', zIndex:'3'}} />
+                <img src={qr} alt="QR Code" className="w-full mb-4" style={{ height: '45vh', width: 'auto', zIndex: '3' }} />
+                {amount && <h4>Payment amount: ₹${amount} /-</h4>}
                 <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={handleCloseClick}>
                     Close
                 </button>
