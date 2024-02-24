ba4a1d54b4f0d44cf500b5b2978d90fcee17e511
Kunal Kumar <kunal23kmr@gmail.com>
2024-02-24 19:22:54 +0530

    background video changed to link.

--- src/USER/HomePage/Homepage.js
+++ src/USER/HomePage/Homepage.js
@@ -6,14 +6,13 @@ import FallingText from '../Backgroundtext/FallingText';
 import Gallery from '../clubs and event/Gallery';
 import Pronightmainpage from '../Pro-Night/Pronightmainpage';
 import LabTabs from '../schedule/Horizontaltab';
-import bv from './background-video.mp4';
 import Sponsors from '../Sponsor/sponsors';
 const Homepage = () => {
     return (
         <>
             <div className="homepage">
                 <video className="background-video" autoPlay muted loop>
-                    <source src={bv} type="video/mp4" />
+                    <source src='https://res.cloudinary.com/dijzvijky/video/upload/v1708782614/InShot_20240221_192422896_xbfzpf.mp4' type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
 
