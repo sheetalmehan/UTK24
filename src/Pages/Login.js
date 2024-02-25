62338dac25c40971b2bdec040a17074119287f4a
ABHISHEK KUMAR <100033618+abhishek110803@users.noreply.github.com>
2024-02-25 21:47:07 +0530

    Updated Login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -47,12 +47,13 @@ const Login = () => {
 
   return (
     // <Layout>
-      <div className="flex items-center justify-center h-[120vh]">
+      <div className="flex items-center justify-center h-[130vh]">
+       
         <form
           onSubmit={handleLogin}
-          className="flex flex-col justify-center gap-4 rounded-lg p-4 text-white w-80 h-[26rem] shadow-[0_0_10px_black]"
+          className="flex flex-col justify-center gap-4 rounded-lg p-4 text-white w-80 h-[30rem] shadow-[0_0_10px_black]"
         >
-          <h1 className="text-center text-2xl font-bold">Login Page</h1>
+           <h1 className="text-center text-2xl font-bold" >Login Page</h1>
           <div className="flex flex-col gap-1">
             <label className="text-lg font-semibold" htmlFor="email">
               Email
@@ -86,14 +87,14 @@ const Login = () => {
           </div>
 
           {/* guest account access */}
-          <div
+          {/* <div
             onClick={() =>
               setLoginData({ email: "test@gmail.com", password: "Test@123" })
             }
             className="text-center  text-accent cursor-pointer"
           >
             Guest Login
-          </div>
+          </div> */}
 
           <button
             className="w-full bg-yellow-600 hover:bg-yellow-500 transition-all ease-in-out duration-300 rounded-sm py-2 font-semibold text-lg cursor-pointer"
